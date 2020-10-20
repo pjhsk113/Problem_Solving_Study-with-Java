5785df2493bfa9f8da2d47cb3ae0a26161b5ae88
민동현 <mindonghyeon890@gmail.com>
2020-10-20 22:57:03 +0900

    feat : 첫번째 문제 완료 - 시간복잡도 O(nlogn) - 공간복잡도 O(1)

---
+++ src/main/java/problem1/Problem1Mdh.java
@@ -0,0 +1,48 @@
+package problem1;
+
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Problem1Mdh {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String[] participant = null;
+        String[] completion = null;
+        String ans = null;
+
+        participant = new String[]{"leo", "kiki", "eden"};
+        completion = new String[]{"eden", "kiki"};
+        ans = "leo";
+        assertThat(solution.solution(participant, completion)).isEqualTo(ans);
+
+        participant = new String[]{"marina", "josipa", "nikola", "vinko", "filipa"};
+        completion = new String[]{"josipa", "filipa", "marina", "nikola"};
+        ans = "vinko";
+        assertThat(solution.solution(participant, completion)).isEqualTo(ans);
+
+        participant = new String[]{"mislav", "stanko", "mislav", "ana"};
+        completion = new String[]{"stanko", "ana", "mislav"};
+        ans = "mislav";
+        assertThat(solution.solution(participant, completion)).isEqualTo(ans);
+
+
+
+        System.out.println("성공");
+    }
+
+    static class Solution {
+        public String solution(String[] participant, String[] completion) {
+            Arrays.sort(participant);
+            Arrays.sort(completion);
+            for (int i = 0; i < completion.length; i++) {
+                if (!participant[i].equals(completion[i])) {
+                    return participant[i];
+                }
+            }
+            return participant[participant.length - 1];
+        }
+    }
+}
