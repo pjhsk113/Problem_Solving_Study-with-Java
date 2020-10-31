fd53d0e3f3cf41263ef67a11e2b1bb12869d264f
pjhsk113 <pjhsk113@naver.com>
2020-10-31 17:42:59 +0900

    [feat] 카펫 패키지 추가

---
+++ src/main/java/카펫/카펫_장호.java
@@ -0,0 +1,40 @@
+package 카펫;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class 카펫_장호 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int brown;
+        int yellow;
+        int[] answer;
+
+        brown = 10;
+        yellow = 2;
+        answer = new int[] {4, 3};
+
+        assertThat(solution.solution(brown, yellow)).containsExactly(answer);
+
+        brown = 8;
+        yellow = 1;
+        answer = new int[] {3, 3};
+
+        assertThat(solution.solution(brown, yellow)).containsExactly(answer);
+
+        brown = 24;
+        yellow = 24;
+        answer = new int[] {8, 6};
+
+        assertThat(solution.solution(brown, yellow)).containsExactly(answer);
+
+
+    }
+
+    static class Solution {
+        public int[] solution(int brown, int yellow) {
+            int[] answer = {};
+            return answer;
+        }
+    }
+}
