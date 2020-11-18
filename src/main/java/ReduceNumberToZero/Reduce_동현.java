79857f4ac034b87de28d45f9c285e15c407d017b
민동현 <mindonghyeon890@gmail.com>
2020-11-18 18:38:16 +0900

    feat : Reduce Number. Solved

--- src/main/java/ReduceNumberToZero/Reduce_동현.java
+++ src/main/java/ReduceNumberToZero/Reduce_동현.java
@@ -23,7 +23,14 @@ public class Reduce_동현 {
     static class Solution {
         public int numberOfSteps (int num) {
             int ans = 0;
-
+            while(num != 0) {
+                if(num % 2 == 0) {
+                    num = num / 2;
+                } else {
+                    num = num - 1;
+                }
+                ans ++;
+            }
             return ans;
         }
     }
