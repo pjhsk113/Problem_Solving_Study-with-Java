0a3a5cb72e7be8bf90193cefe8d6a837bf7d3d8c
JanghoPark <pjhsk113@naver.com>
2021-08-19 18:09:57 +0900

    feat: 프로그래머스 Level1 - 이상한 문자 만들기 solved

---
+++ src/main/java/프로그래머스/이상한_문자_만들기.java
@@ -0,0 +1,37 @@
+package 프로그래머스;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class 이상한_문자_만들기 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        String s;
+        String result;
+
+        s = "try hello world";
+        result = "TrY HeLlO WoRlD";
+
+        assertThat(solution.solution(s)).isEqualTo(result);
+
+        s = "a b c d e";
+        result = "A B C D E";
+
+        assertThat(solution.solution(s)).isEqualTo(result);
+
+    }
+    static class Solution {
+        public String solution(String s) {
+            String answer = "";
+            String[] arr = s.split("");
+            int cnt = 0;
+
+            for (String word : arr) {
+                cnt = word.equals(" ") ? 0 : ++cnt;
+                answer += cnt % 2 == 0 ? word.toLowerCase() : word.toUpperCase();
+            }
+
+            return answer;
+        }
+    }
+}
