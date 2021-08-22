ef61e507f9c3a988b7e04126b3c77bb8b43df3b2
JanghoPark <pjhsk113@naver.com>
2021-08-22 15:02:41 +0900

    feat: 프로그래머스 Level1 - 2016년 solved

---
+++ src/main/java/프로그래머스/달력_2016년.java
@@ -0,0 +1,43 @@
+package 프로그래머스;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class 달력_2016년 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int a;
+        int b;
+        String result;
+
+        a = 5;
+        b = 24;
+        result = "TUE";
+
+        assertThat(solution.solution(a, b)).isEqualTo(result);
+
+        a = 1;
+        b = 1;
+        result = "FRI";
+
+        assertThat(solution.solution(a, b)).isEqualTo(result);
+
+    }
+    static class Solution {
+        public String solution(int a, int b) {
+            String answer = "";
+            String[] days = new String[]{"THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"};
+            int[] month = new int[] {31,29,31,30,31,30,31,31,30,31,30,31};
+            int dayCount = 0;
+
+            for (int i = 0; i < a-1; i++) {
+                dayCount += month[i];
+            }
+            dayCount += b;
+
+            answer = days[dayCount % 7];
+            return answer;
+        }
+    }
+}
