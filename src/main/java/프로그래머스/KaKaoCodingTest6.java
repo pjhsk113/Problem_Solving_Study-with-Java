b90d164ccf5ea7bf9cd4d492bc9f7dbfc5f12005
JanghoPark <pjhsk113@naver.com>
2021-09-11 19:07:14 +0900

    feat: 카카오 2021 블라인드 코딩테스트 6번 - 정확성만 통과 효율성 폭망!

---
+++ src/main/java/프로그래머스/KaKaoCodingTest6.java
@@ -0,0 +1,59 @@
+package 프로그래머스;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class KaKaoCodingTest6 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[][] board;
+        int[][] skill;
+        int ans;
+
+        board = new int[][]{{5,5,5,5,5},{5,5,5,5,5},{5,5,5,5,5},{5,5,5,5,5}};
+        skill = new int[][]{{1,0,0,3,4,4},{1,2,0,2,3,2},{2,1,0,3,1,2},{1,0,1,3,3,1}};
+        ans = 10;
+        assertThat(solution.solution(board, skill)).isEqualTo(ans);
+
+    }
+
+    static class Solution {
+        public int solution(int[][] board, int[][] skill) {
+            int answer = 0;
+            for (int i = 0; i < skill.length; i++) {
+                int type = skill[i][0];
+                int start = skill[i][1];
+                int secondStart = skill[i][2];
+                int firstRange = skill[i][3];
+                int secondRange = skill[i][4];
+                int degree = skill[i][5];
+                board = getBoards(type, start, secondStart, degree, firstRange, secondRange, board);
+            }
+
+            for (int i = 0; i < board.length; i++) {
+                for (int j = 0; j < board[0].length; j++) {
+                    if (board[i][j] > 0) {
+                        answer++;
+                    }
+                }
+                System.out.println();
+            }
+            return answer;
+        }
+
+        static int[][] getBoards(int type, int start, int secondStart, int degree, int firstRange, int secondRange, int[][] board) {
+            for (int i = start; i <= firstRange; i++) {
+                for (int j = secondStart; j <= secondRange; j++) {
+                    if (type == 1) {
+                        board[i][j] -= degree;
+                    } else {
+                        board[i][j] += degree;
+                    }
+                }
+            }
+
+            return board;
+        }
+    }
+
+}
